b399a3c268e61c5c081b31e9321bbdd885ad8664
ahmed-aa1801564 <66021968+AhmedAmmar-ge53qup@users.noreply.github.com>
2024-06-01 01:03:08 +0200

    Now Redirects to correct page

--- assignment03/app/page.jsx
+++ assignment03/app/page.jsx
@@ -1,6 +1,18 @@
+"use client"
 import styles from './page.module.css'
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect the user to '/ideas' after the component has mounted
+    router.push('/words');
+  }, []); // The empty dependency array ensures that this effect runs only once after the component mounts
+
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
--- assignment04/app/page.jsx
+++ assignment04/app/page.jsx
@@ -1,14 +1,26 @@
+"use client"
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect the user to '/ideas' after the component has mounted
+    router.push('/ideas');
+  }, []); // The empty dependency array ensures that this effect runs only once after the component mounts
+
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        
+
       </div>
     </main>
   )
